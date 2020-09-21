b1fa844f6f7a3f104f9bdd25d09e291e9a5f0b25
Christian Gram Kalhauge <christian@kalhauge.dk>
2020-09-21 16:10:24 +0200

    Initial commit

---
+++ src/main/java/chadchat/entries/Server.java
@@ -0,0 +1,10 @@
+package chadchat.entries;
+
+public class Server {
+    // The entry point of the ChatChad server
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/chadchat/entries/ServerTest.java
@@ -0,0 +1,7 @@
+package chadchat.entries;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+}
