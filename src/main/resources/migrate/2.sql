9babaff49b65f9b8997cf428fdc47ebf6946ac63
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-09-27 20:24:11 +0200

    Menu.java moved to main

--- src/main/resources/migrate/2.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS animals;
-CREATE TABLE animals (
-    id int PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(25),
-    birthday DATE,
-    type int
-);
-
