3e2fe87cb7df97cca057e5d3787bf43427649226
Peter-Rambeck <peter@indochinaexplorers.com>
2020-09-29 11:00:23 +0200

    Database

--- src/main/java/chadchat/domain/Channel.java
+++ src/main/java/chadchat/domain/Channel.java
@@ -1,28 +1,38 @@
 package chadchat.domain;
 
+import java.time.LocalDateTime;
+
 public class Channel {
 
-    private int id;
-    private String name;
+    private final int id;
+    private final String channelName;
+    private final LocalDateTime timestamp;
 
-    public Channel(int id, String name) {
+
+    public Channel(int id, String channelName, LocalDateTime timestamp) {
         this.id = id;
-        this.name = name;
+        this.channelName = channelName;
+        this.timestamp = timestamp;
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getChannelName() {
+        return channelName;
     }
 
-    public String getName() {
-        return name;
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", Channel name='" + channelName + '\'' +
+                ", createdAt=" + timestamp +
+                '}';
     }
 }
