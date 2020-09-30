b1e6c063f851b05c45faacaaae2c60b290482b55
Janus <Janussr@gmail.com>
2020-09-30 16:01:55 +0200

    Created logout method

--- src/main/java/chadchat/api/ChadChat.java
+++ src/main/java/chadchat/api/ChadChat.java
@@ -11,32 +11,38 @@ public class ChadChat {
     private final Set<MessageObserver> messageObservers = new HashSet<>();
     private final Set<User> activeUsers = new HashSet<>();
     private final Database db = new Database();
-    
-    public User userLogin(String username){
-        return db.checkLogin(username);
+
+    public User userLogin(String username) {
+        User user = db.checkLogin(username);
+        activeUsers.add(user);
+        return user;
     }
-    
+
+    public void logout(User user) {
+        activeUsers.remove(user);
+    }
+
     public void createMessage(User user, String msg) {
-        db.createMessage(msg,user);
+        db.createMessage(msg, user);
         synchronized (this) {
             for (MessageObserver messageObserver : messageObservers) {
                 messageObserver.notifyNewMessages();
             }
         }
     }
-    
+
     public Iterable<Message> getNewMessages(int lastSeenMsg) {
         // Database get messages
         return db.findAllMessages(lastSeenMsg);
     }
-    
+
     public synchronized void registerMessageObserver(MessageObserver observer) {
         messageObservers.add(observer);
     }
-    
-    
+
+
     public interface MessageObserver {
         void notifyNewMessages();
     }
-    
+
 }
